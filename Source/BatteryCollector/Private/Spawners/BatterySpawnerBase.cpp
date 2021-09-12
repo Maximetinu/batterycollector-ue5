3a623e7e5f086f2c9ffb17a615985660f0a475ba
Miguel Medina Ballesteros <maximetinu@gmail.com>
2021-09-12 14:09:21 +0100

    Create BatterySpawnerBase class

---
+++ Source/BatteryCollector/Private/Spawners/BatterySpawnerBase.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Spawners/BatterySpawnerBase.h"
+
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values
+ABatterySpawnerBase::ABatterySpawnerBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABatterySpawnerBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABatterySpawnerBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
