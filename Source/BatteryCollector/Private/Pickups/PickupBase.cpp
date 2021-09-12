51ddd2c01af9546944b26aaa26cd4c17a6e5bdaf
Miguel Medina Ballesteros <maximetinu@gmail.com>
2021-09-12 03:20:21 +0100

    Initialize PickupBase mesh

--- Source/BatteryCollector/Private/Pickups/PickupBase.cpp
+++ Source/BatteryCollector/Private/Pickups/PickupBase.cpp
@@ -9,6 +9,8 @@ APickupBase::APickupBase()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pickup Mesh"));
+	RootComponent = PickupMesh;
 }
 
 // Called when the game starts or when spawned
